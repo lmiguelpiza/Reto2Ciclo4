9fa449167b2b0b5b8ee9d5540d237dcb7aa4b98a
Miguel_Piza <luis.piza.mt@correo.usa.edu.co>
2021-12-02 20:43:54 -0500

    hola commit

---
+++ src/main/java/com/ciclo4/reto/controlador/UserController.java
@@ -0,0 +1,48 @@
+package com.ciclo4.reto.controlador;
+
+
+import com.ciclo4.reto.modelo.User;
+import com.ciclo4.reto.servicio.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin("*")
+
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all")
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User update(@RequestBody User user) {
+        return userService.update(user);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id") int id) {
+        return userService.delete(id);
+    }
+    @GetMapping("/{email}/{password}")
+    public User authenticateUser(@PathVariable("email") String email, @PathVariable("password") String password) {
+        return userService.authenticateUser(email, password);
+    }
+    @GetMapping("/emailexist/{email}")
+    public boolean emailExists(@PathVariable("email") String email) {
+        return userService.emailExists(email);
+    }
+}
